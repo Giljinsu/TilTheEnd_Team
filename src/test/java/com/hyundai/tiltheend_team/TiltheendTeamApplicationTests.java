5147d8fefb77e3136d8aac3f156cbf0cda7ea51d
Giljinsu <therin234@gmail.com>
2022-12-27 14:32:49 +0900

    maven_commit

---
+++ src/test/java/com/hyundai/tiltheend_team/TiltheendTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyundai.tiltheend_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiltheendTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
