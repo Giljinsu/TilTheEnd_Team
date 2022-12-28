cd4002abf0ce7e671f21864aa0cad22f6aabf36c
Giljinsu <therin234@gmail.com>
2022-12-28 11:56:37 +0900

    add Filter

---
+++ src/main/java/com/hyundai/tiltheend_team/dao/InsertUsers.java
@@ -0,0 +1,11 @@
+package com.hyundai.tiltheend_team.dao;
+
+import java.sql.Statement;
+
+public class InsertUsers {
+    public void inserUser() {
+        Common common = new Common();
+        Statement statement  = common.getStatement();
+        
+    }
+}
--- src/main/java/com/hyundai/tiltheend_team/servlets/JoinMemberServlet.java
+++ src/main/java/com/hyundai/tiltheend_team/servlets/JoinMemberServlet.java
@@ -1,9 +1,21 @@
 package com.hyundai.tiltheend_team.servlets;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/joinMemberServlet")
 public class JoinMemberServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getAttribute("name");
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
 }
