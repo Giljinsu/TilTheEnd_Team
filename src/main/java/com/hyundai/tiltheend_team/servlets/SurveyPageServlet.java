148ff54b78409f9cd9129344f603d5e8e4eae4cc
yongbeom76 <yongbeom76@gmail.com>
2022-12-28 11:50:27 +0900

    add servlets

---
+++ src/main/java/com/hyundai/tiltheend_team/servlets/SurveyPageServlet.java
@@ -0,0 +1,9 @@
+package com.hyundai.tiltheend_team.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/surveyPageServlet")
+public class SurveyPageServlet extends HttpServlet {
+
+}
