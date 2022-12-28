7654420ac3d31c7977d9326118fa2a6d5b5595e4
kimbyeonggeun <qudrms2157@gmail.com>
2022-12-28 14:30:36 +0900

    add files

--- src/main/java/com/hyundai/tiltheend_team/dao/SurveyPageDB.java
+++ src/main/java/com/hyundai/tiltheend_team/dao/SurveyPageDB.java
@@ -10,12 +10,15 @@ public class SurveyPageDB {
     public ArrayList<HashMap> getAnswersList(String questionUid) throws SQLException {
         Common common = new Common();
         Statement statement = common.getStatement();
-        String query = "";
+        String query = "SELECT SURVEY.QUESTION_ID, ANSWER.ORDERS, ANSWER.ANSWER" +
+                "FROM SURVEY INNER JOIN ANSWER" +
+                "ON SURVEY.ANSWER_ID = ANSWER.ANSWER_ID" +
+                "ORDER BY QUESTION_ID";
         ResultSet resultSet = statement.executeQuery(query);
         ArrayList<HashMap> answerList = new ArrayList<HashMap>();
         while (resultSet.next()) {
             HashMap<String, Object> answer = new HashMap<String, Object>();
-
+            answer.put("QUESTION_ID", resultSet.getString("QUESTION_ID"));
         }
         return answerList;
     }
