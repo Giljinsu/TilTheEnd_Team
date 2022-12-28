2e2715df4ea53dd4af555ae0f189c56fe8ffe7a8
kimbyeonggeun <qudrms2157@gmail.com>
2022-12-28 14:43:09 +0900

    임시 완성

--- src/main/java/com/hyundai/tiltheend_team/dao/SurveyPageDB.java
+++ src/main/java/com/hyundai/tiltheend_team/dao/SurveyPageDB.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SurveyPageDB {
-    public ArrayList<HashMap> getAnswersList(String questionUid) throws SQLException {
+    public ArrayList<HashMap> getAnswersList() throws SQLException {
         Common common = new Common();
         Statement statement = common.getStatement();
         String query = "SELECT SURVEY.QUESTION_ID, ANSWER.ORDERS, ANSWER.ANSWER" +
@@ -19,7 +19,24 @@ public class SurveyPageDB {
         while (resultSet.next()) {
             HashMap<String, Object> answer = new HashMap<String, Object>();
             answer.put("QUESTION_ID", resultSet.getString("QUESTION_ID"));
+            answer.put("ORDERS", resultSet.getString("ORDERS"));
+            answer.put("ANSWER", resultSet.getString("ANSWER"));
+            answerList.add(answer);
         }
         return answerList;
+    }
+
+    public HashMap<String, Object> getQuestion() throws SQLException {
+        Common common = new Common();
+        Statement statement = common.getStatement();
+        String query = "SELECT * FROM QUESTION";
+        ResultSet resultSet = statement.executeQuery(query);
+        HashMap<String, Object> result = null;
+        while (resultSet.next()) {
+            result.put("QUESTION_ID", resultSet.getString("QUESTION_ID"));
+            result.put("ORDERS", resultSet.getString("ORDERS"));
+            result.put("ANSWER", resultSet.getInt("ANSWER"));
+        }
+        return result;
     }
 }
