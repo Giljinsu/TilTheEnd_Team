b437d8a1a11f8b7e9869ef71c5fa9636d1d7d865
Giljinsu <therin234@gmail.com>
2023-01-02 21:14:05 +0900

    add RemovePage

---
+++ src/main/java/com/hyundai/tiltheend_team/dao/RemoveSurveyDao.java
@@ -0,0 +1,36 @@
+package com.hyundai.tiltheend_team.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RemoveSurveyDao {
+    public Boolean removeSurvey(String userId, String password) {
+        Common common = new Common();
+        Statement statement = common.getStatement();
+        String query = "SELECT USER_PW FROM users WHERE USER_ID = '"+userId+"'";
+        String dbPW = "";
+        try {
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()) {
+                dbPW = resultSet.getString("USER_PW");
+            }
+            if(password.equals(dbPW)) {
+                query = "DELETE FROM users_answer WHERE USER_ID = '"+userId+"'";
+                statement.execute(query);
+                return true;
+            }else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } 
+        return false;
+    }
+}
