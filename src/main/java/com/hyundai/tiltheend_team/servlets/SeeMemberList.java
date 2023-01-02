a8bc2d00808ca094290a28ebec69545fd4a9145d
yongbeom76 <yongbeom76@gmail.com>
2023-01-02 13:36:00 +0900

    seeMemberList.java

---
+++ src/main/java/com/hyundai/tiltheend_team/servlets/SeeMemberList.java
@@ -0,0 +1,17 @@
+package com.hyundai.tiltheend_team.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/seeMemberList")
+public class SeeMemberList extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
